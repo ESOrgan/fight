8114b7a095d256943212ef5d0d76041645528281
Infinity-Energy <59878199+Infinity-Energy@users.noreply.github.com>
2021-05-12 20:56:30 +0800

    delete extra space

--- settings.py
+++ settings.py
@@ -2,12 +2,12 @@ REBIRTH = False
 
 auto_save = False  # test function
 
-PROPERTY_KEY = ["max_mana", "mana", "mana_reg", "str", "max_str", "str_reg"]
+PROPERTY_KEY = ["max_mana", "mana", "mana_reg", "str", "max_str", "str_reg", "place", "km"]
 PROPERTY_EXPR = {
     "max_mana": "player_property['max_mana'] = player_property['lv'] * 10 + 20",
     "mana": "player_property['mana'] = player_property['max_mana']",
     "mana_reg": "player_property['mana_reg'] = int(player_property['lv'] / 5) + 1",
     "str": 'player_property["str"] = player_property["lv"] * 35 + 15',
     "max_str": 'player_property["max_str"] = player_property["str"]',
-    "str_reg": 'player_property["str_reg"] = int(player_property["lv"] * 0.1    ) + 1'
+    "str_reg": 'player_property["str_reg"] = int(player_property["lv"] * 0.1) + 1'
 }
